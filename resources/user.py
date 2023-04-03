5b95a568575bd1af67dae85093f6c7b5c3b0b951
fernando predes <fernando_predes@hotmail.com>
2023-04-02 23:57:54 -0300

    adicionado o user.id junto com o token

--- resources/user.py
+++ resources/user.py
@@ -42,7 +42,7 @@ class UserLogin(MethodView):
 
         if user and pbkdf2_sha256.verify(user_data['password'], user.password):
             access_token = create_access_token(identity=user.id)
-            return {"access_token": access_token}
+            return {"access_token": access_token, "user_id": user.id}
 
         abort(401, message="Invalid cedentials.")
 
