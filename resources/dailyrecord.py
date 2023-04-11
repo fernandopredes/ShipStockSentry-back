59ba4f3f25a588f885bb4497b1cfb316cff097c6
fernando predes <fernando_predes@hotmail.com>
2023-04-10 21:13:40 -0300

    adicionadas mais descrições no controller de dailyrecord

--- resources/dailyrecord.py
+++ resources/dailyrecord.py
@@ -13,34 +13,46 @@ blp = Blueprint("Daily Records", __name__, description="Operations on Daily Reco
 @blp.route('/daily_record')
 class DailyRecordList(MethodView):
     @jwt_required()
-    @blp.doc(summary="Rota para postar um daily_record")
     @blp.arguments(DailyRecordSchema)
     @blp.response(201, DailyRecordSchema)
     def post(self, daily_record_data):
+        """Rota para postar um daily_record
+
+        Retorna uma representação das informações enviadas de um daily record.
+
+        """
         daily_record = DailyRecordModel(**daily_record_data)
 
         try:
             db.session.add(daily_record)
             db.session.commit()
         except SQLAlchemyError:
-            abort(500, message="An error occurred while inserting a Daily Record.")
+            abort(500, message="Um erro ocorreu ao tentar criar um Daily Record.")
 
         return daily_record
 
 @blp.route('/daily_record/<int:daily_record_id>')
 class DailyRecord(MethodView):
-    @blp.doc(summary="Rota para pegar um daily_record pelo id")
     @blp.response(200, DailyRecordSchema)
     @jwt_required()
     def get(self, daily_record_id):
+        """ Rota para pegar um daily_record pelo id
+
+        Retorna uma representação das informações recebidas de um daily record.
+
+        """
         daily_record = DailyRecordModel.query.get_or_404(daily_record_id)
         return daily_record
 
     @jwt_required()
-    @blp.doc(summary="Rota para editar um daily_record de acordo com o id")
     @blp.arguments(UpdateRecordSchema)
     @blp.response(200, DailyRecordSchema)
     def put(self, daily_record_data, daily_record_id):
+        """ Rota para editar um daily_record de acordo com o id
+
+        Retorna uma representação das informações editadas de um daily record.
+
+        """
         daily_record = DailyRecordModel.query.get(daily_record_id)
         if daily_record:
             if daily_record_data.get('date') is not None:
@@ -60,8 +72,12 @@ class DailyRecord(MethodView):
         return daily_record
 
     @jwt_required()
-    @blp.doc(summary="Rota para deletar um daily_record pelo id")
     def delete(self, daily_record_id):
+        """ Rota para deletar um daily_record pelo id
+
+        Retorna uma mensagem confirmando que o daily_record selecionado foi deletado.
+
+        """
         daily_record = DailyRecordModel.query.get_or_404(daily_record_id)
         db.session.delete(daily_record)
         db.session.commit()
